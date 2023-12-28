9e7d21626e06fa43f2ddf893a5e50f9985c833e7
Manjunath <sreerammanjunath1392@gmail.com>
2023-12-28 22:09:18 +0530

    'Version-3'

--- js.js
+++
@@ -1,7 +0,0 @@
-
-setTimeout(() => {
-    // document.body.style.background = "linear-gradient(to right, #a80077, #66ff00);"
-    document.body.style.background = "linear-gradient(to right, #a80077, #66ff00);"
-}, 2000);
-
-console.log('working')
